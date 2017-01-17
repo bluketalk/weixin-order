01ff0a1ba247079f94247897852f606a89924d55
li.yc <lyc@guagua.local>
2017-01-17 16:24:22 +0800

    no message

--- pages/classify/index.js
+++ pages/classify/index.js
@@ -9,21 +9,6 @@ Page({
     onLoad: function() {
 
         var that = this
-        
-        // wx.request({
-        //     url: 'http://huanqiuxiaozhen.com/wemall/goodstype/typebrandList',
-        //     method: 'GET',
-        //     data: {},
-        //     header: {
-        //         'Accept': 'application/json'
-        //     },
-        //     success: function(res) {
-        //         that.setData({
-        //             navLeftItems: res.data,
-        //             navRightItems: res.data
-        //         })
-        //     }
-        // })
     },
 
     //事件处理函数
